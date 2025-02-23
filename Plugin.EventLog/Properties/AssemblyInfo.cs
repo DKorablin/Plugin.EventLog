1b5596221122ac8627d03fdffe25e28306c9e5a9
DKorablin2 <danila.korablin@unity3d.com>
2025-02-23 23:37:09 +0300

    Unified plugins deployment

--- Plugin.EventLog/Properties/AssemblyInfo.cs
+++ Plugin.EventLog/Properties/AssemblyInfo.cs
@@ -1,7 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: ComVisible(false)]
 [assembly: Guid("535b6be7-847b-45ab-bdaa-68e1e52be508")]
 [assembly: System.CLSCompliant(false)]
 
@@ -9,14 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyMetadata("ProjectUrl", "https://github.com/DKorablin/Plugin.EventLog")]
 #else
 
-[assembly: AssemblyTitle("Plugin.EventLog")]
 [assembly: AssemblyDescription("Simple plugin to monitor EventLog events")]
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
-[assembly: AssemblyCompany("Danila Korablin")]
-[assembly: AssemblyProduct("Plugin.EventLog")]
 [assembly: AssemblyCopyright("Copyright © Danila Korablin 2015-2017")]
 #endif
